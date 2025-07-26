07f2013d5dc2394be743532c92c5c968397972e8
Srijan Nayak <srijannayak0@gmail.com>
2025-07-26 11:59:00 +0530

    Restrict current patterns to only GET method

--- main.go
+++ main.go
@@ -28,9 +28,9 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
 
 	log.Println("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
