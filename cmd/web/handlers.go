b5f96c5adbfd097c6aceda946929d05dc1222881
Srijan Nayak <srijannayak0@gmail.com>
2025-08-03 20:48:51 +0530

    Finish user signup handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -124,7 +124,23 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintln(w, "Display a form for logging in a user...")
+	err = app.users.Insert(form.Name, form.Email, form.Password)
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicatedEmail) {
+			form.AddFieldError("email", "Email address is already in use")
+
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, r, http.StatusUnprocessableEntity, "signup.gohtml", data)
+		} else {
+			app.serverError(w, r, err)
+		}
+
+		return
+	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Your signup was successful. Please log in.")
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
--- internal/models/users.go
+++ internal/models/users.go
@@ -2,6 +2,10 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
 	"time"
 )
 
@@ -18,6 +22,27 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `
+		INSERT INTO users (name, email, hashed_password, created)
+		VALUES (?, ?, ?, UTC_TIMESTAMP())
+	`
+
+	_, err = m.DB.Exec(stmt, name, email, string(hasedPassword))
+	if err != nil {
+		var mySQLError *mysql.MySQLError
+		if errors.As(err, &mySQLError) {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users_uc_email") {
+				return ErrDuplicatedEmail
+			}
+		}
+		return err
+	}
+
 	return nil
 }
 
